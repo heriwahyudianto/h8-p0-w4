3b6d0c4b078b8ae2624be9ba41f8f552783794a4
heri <heriwahyudianto@gmail.com>
2020-02-08 22:53:45 +0700

    ubah huruf tanpa magic

--- exercise-5.js
+++ exercise-5.js
@@ -1,37 +1,16 @@
 function ubahHuruf(kata) {
   // you can only write your code here!
-  arrKata = kata.split('');
-  for (i = 0; i < arrKata.length; i++) {    
-    switch (kata[i]) {
-      case 'a' : arrKata[i] = 'b'; break;
-      case 'b' : arrKata[i] = 'c'; break;
-      case 'c' : arrKata[i] = 'd'; break;
-      case 'd' : arrKata[i] = 'e'; break;
-      case 'e' : arrKata[i] = 'f'; break;
-      case 'f' : arrKata[i] = 'g'; break;
-      case 'g' : arrKata[i] = 'h'; break;
-      case 'h' : arrKata[i] = 'i'; break;
-      case 'i' : arrKata[i] = 'j'; break;
-      case 'j' : arrKata[i] = 'k'; break;
-      case 'k' : arrKata[i] = 'l'; break;
-      case 'l' : arrKata[i] = 'm'; break;
-      case 'm' : arrKata[i] = 'n'; break;
-      case 'n' : arrKata[i] = 'o'; break;
-      case 'o' : arrKata[i] = 'p'; break;
-      case 'p' : arrKata[i] = 'q'; break;
-      case 'q' : arrKata[i] = 'r'; break;
-      case 'r' : arrKata[i] = 's'; break;
-      case 's' : arrKata[i] = 't'; break;
-      case 't' : arrKata[i] = 'u'; break;
-      case 'u' : arrKata[i] = 'v'; break;
-      case 'v' : arrKata[i] = 'w'; break;
-      case 'w' : arrKata[i] = 'x'; break;
-      case 'x' : arrKata[i] = 'y'; break;
-      case 'y' : arrKata[i] = 'z'; break;
-      case 'z' : arrKata[i] = 'a'; break;
+  let hash = '';
+  const KAMUS = 'abcdefghijklmnopqrstuvwxyza';
+  for (j = 0; j < kata.length; j++) {
+    for (i = 0; i < KAMUS.length; i++) {
+      if (kata[j] === KAMUS[i]) {
+        hash += KAMUS[i + 1]; 
+        break;
+      }
     }
-  }
-  return arrKata.join('');
+  }      
+  return hash;
 }
 
 // TEST CASES
